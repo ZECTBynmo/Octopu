e16632444deea4a32a6a9a9c4e40559a666de16e
ZECTBynmo <michael.vegeto@gmail.com>
2013-12-29 02:56:28 -0500

    New controller for main starter functionality

---
+++ starter/api/controllers/StarterController.js
@@ -0,0 +1,34 @@
+/**
+ * StarterController
+ *
+ * @module      :: Controller
+ * @description	:: A set of functions called `actions`.
+ *
+ *                 Actions contain code telling Sails how to respond to a certain type of request.
+ *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
+ *
+ *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
+ *                 and/or override them with custom routes (`config/routes.js`)
+ *
+ *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
+ *
+ * @docs        :: http://sailsjs.org/#!documentation/controllers
+ */
+
+module.exports = {
+    
+ 	index: function( req, res ) {
+    	res.json( 200, {} );
+    },
+
+    setConfig: function( req, res ) {
+
+    	var responseObj = {
+    		"error": undefined,
+    		"url": req.body.url
+    	};
+
+    	res.view( responseObj );
+    }
+  
+};
